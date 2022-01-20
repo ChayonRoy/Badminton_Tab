c8f4359ccea2a110eef41f1ecc536399563ebf78
Chayon Roy <chayonroy.khulna@gmail.com>
2022-01-20 18:32:51 +0600

    5th commit

--- app/src/main/java/com/chayonroy/BadmintonTab/DoublesActivity.java
+++ app/src/main/java/com/chayonroy/BadmintonTab/DoublesActivity.java
@@ -8,15 +8,19 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class DoublesActivity extends AppCompatActivity {
 
-    Button pointa, pointb, undoa, undob, btn;
+    Button pointa, pointb, undoa, undob, A_2nd_srv,B_2nd_srv;
+    ToggleButton sideout;
 
     TextView ascore, bscore, acenter, bcenter;
     TextView aplayer1,aplayer2,bplayer1,bplayer2,teamA, teamB;
-    int count_a, count_b;
+
+    int count_a, count_b, clr_count_A, clr_count_B;
 
     String pl1a, pl2a, pl1b, pl2b;
     CardView alft, aright, blft, bright;
@@ -31,7 +35,9 @@ public class DoublesActivity extends AppCompatActivity {
         undoa=findViewById(R.id.undo_a);
         pointb=findViewById(R.id.point_b);
         undob=findViewById(R.id.undo_b);
-        btn=findViewById(R.id.button);
+        A_2nd_srv=findViewById(R.id.button);
+        B_2nd_srv=findViewById(R.id.B_2nd_service);
+        sideout=findViewById(R.id.side_out);
 
         ascore =findViewById(R.id.ascore);
         bscore = findViewById(R.id.BScore);
@@ -70,21 +76,17 @@ public class DoublesActivity extends AppCompatActivity {
         teamB.setText("Team B: "+ p1b + " & "+p2b);
 
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(DoublesActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
 
         pointa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 count_a++;
+                clr_count_A++;
                 ascore.setText(count_a+"");
                 acenter.setText(count_a+"");
                 teama_crtinfo();
+                cort_clr_A();
 
             }
         });// ...................................................Button Point A
@@ -92,9 +94,11 @@ public class DoublesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 count_a--;
+                clr_count_A--;
                 ascore.setText(count_a+"");
                 acenter.setText(count_a+"");
                 teama_crtinfo();
+                cort_clr_A();
 
             }
         });// ....................................................Button undo A
@@ -102,9 +106,11 @@ public class DoublesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 count_b++;
+                clr_count_B++;
                 bscore.setText(count_b+"");
                 bcenter.setText(count_b+"");
                 teamb_crtinfo();
+                cort_clr_B();
 
             }
         }); // ..................................................Button Point B
@@ -113,15 +119,53 @@ public class DoublesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 count_b--;
+                clr_count_B--;
                 bscore.setText(count_b+"");
                 bcenter.setText(count_b+"");
                 teamb_crtinfo();
+                cort_clr_B();
             }
         });// ....................................................Button undo B
 
+        A_2nd_srv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                clr_count_A++;
+                cort_clr_A();
+                // Intent intent= new Intent(DoublesActivity.this, MainActivity.class);
+               // startActivity(intent);
+            }
+        });//....................................................Team A 2nd service
+
+        B_2nd_srv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                clr_count_B++;
+                cort_clr_B();
+            }
+        });//....................................................Team B 2nd service
+
+        sideout.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    clr_count_A=0;
+                    aright.setCardBackgroundColor(Color.parseColor("#035409"));
+                    alft.setCardBackgroundColor(Color.parseColor("#035409"));
+                    bright.setCardBackgroundColor(Color.parseColor("#f65151"));
+                } else {
+                    clr_count_B=0;
+                    blft.setCardBackgroundColor(Color.parseColor("#035409"));
+                    bright.setCardBackgroundColor(Color.parseColor("#035409"));
+                    aright.setCardBackgroundColor(Color.parseColor("#f65151"));
+                }
+            }
+        });
 
 
-    }//onCreate
+
+    }//..............End of onCreate
 
     public void teama_crtinfo()
     {
@@ -130,17 +174,30 @@ public class DoublesActivity extends AppCompatActivity {
         {
             aplayer1.setText(pl1a+"");
             aplayer2.setText(pl2a+"");
-            aright.setCardBackgroundColor(Color.parseColor("#f65151"));
-            alft.setCardBackgroundColor(Color.parseColor("#035409"));
+            //aright.setCardBackgroundColor(Color.parseColor("#f65151"));
+            //alft.setCardBackgroundColor(Color.parseColor("#035409"));
         }
         else if (count_a % 2==1)
         {
             aplayer1.setText(pl2a+"");
             aplayer2.setText(pl1a+"");
+            //alft.setCardBackgroundColor(Color.parseColor("#f65151"));
+            //aright.setCardBackgroundColor(Color.parseColor("#035409"));
+        }
+    }// ................................Player name change Function of Team A
+
+    public void cort_clr_A()
+    {
+        if (clr_count_A % 2==0) {
+            aright.setCardBackgroundColor(Color.parseColor("#f65151"));
+            alft.setCardBackgroundColor(Color.parseColor("#035409"));
+        }
+        else if(clr_count_A % 2==1)
+        {
             alft.setCardBackgroundColor(Color.parseColor("#f65151"));
             aright.setCardBackgroundColor(Color.parseColor("#035409"));
         }
-    }// ................................Player name change Function of Team A
+    }//......................................cort color change by point.
 
     public void teamb_crtinfo()
     {
@@ -149,16 +206,28 @@ public class DoublesActivity extends AppCompatActivity {
         {
             bplayer1.setText(pl1b+"");
             bplayer2.setText(pl2b+"");
-            bright.setCardBackgroundColor(Color.parseColor("#f65151"));
-            blft.setCardBackgroundColor(Color.parseColor("#035409"));
+           // bright.setCardBackgroundColor(Color.parseColor("#f65151"));
+            //blft.setCardBackgroundColor(Color.parseColor("#035409"));
         }
         else if (count_b % 2==1)
         {
             bplayer1.setText(pl2b+"");
             bplayer2.setText(pl1b+"");
+           // blft.setCardBackgroundColor(Color.parseColor("#f65151"));
+           // bright.setCardBackgroundColor(Color.parseColor("#035409"));
+        }
+    }// ................................Player name change Function of Team B
+    public void cort_clr_B()
+    {
+        if (clr_count_B % 2==0) {
+            bright.setCardBackgroundColor(Color.parseColor("#f65151"));
+            blft.setCardBackgroundColor(Color.parseColor("#035409"));
+        }
+        else if(clr_count_B % 2==1)
+        {
             blft.setCardBackgroundColor(Color.parseColor("#f65151"));
             bright.setCardBackgroundColor(Color.parseColor("#035409"));
         }
-    }// ................................Player name change Function of Team B
+    }//......................................cort color change by point.
 
 }// AppCompatActivity
